a0536321b545db6338b6b3026c6e6a17f50b19b2
pzender <ememek96@gmail.com>
2018-04-08 22:58:01 +0200

    basic layout set up

---
+++ BuildsOfSkyrim/Models/Build.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BuildsOfSkyrim.Models
+{
+    public class Build
+    {
+        public string Name;
+        public float StatHealth, StatMagicka, StatStamina;
+        public string Description;
+        public string ImgLink;
+    }
+}
